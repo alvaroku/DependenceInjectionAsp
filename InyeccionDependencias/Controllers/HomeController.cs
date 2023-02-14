4ce24a2ce049ebdbbfe3095842fe26b6344af36c
Alvaro Kú <alvaroku123@gmail.com>
2023-02-14 16:21:02 -0600

    ok

---
+++ InyeccionDependencias/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using InyeccionDependencias.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InyeccionDependencias.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _appDbContext;
+
+        public HomeController(ILogger<HomeController> logger, AppDbContext appDbContext)
+        {
+            _logger = logger;
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_appDbContext.Usuarios.ToList());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Usuario usuario)
+        {
+            _appDbContext.Usuarios.Add(usuario);
+            _appDbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
